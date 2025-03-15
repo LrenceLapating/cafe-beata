3b8df88f522ee20552ead48eb83122550e0b61e9
LrenceLapating <mlapating_220000001421@uic.edu.ph>
2025-03-15 15:04:08 +0800

    Updated march fast.api for rendesr

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
   transpileDependencies: [],
   devServer: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: process.env.VUE_APP_API_URL || 'http://localhost:8000',
         changeOrigin: true
       }
     }
-  }
-});
+  },
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/'
+    : '/'
+})
